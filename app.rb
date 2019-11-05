3be7f879c4ce6df164e511e46b357f81a6fbfb13
Karen V <karenvo@usc.edu>
2019-11-05 07:45:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,9 +17,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     # repeat phrase n times
     @phrase1 = params[:phrase]
-    @number = params[:number].to_i
+    @num1 = params[:number].to_i
     times = 0
-    until times == @number do
+    until times == @num1 do
       "#{@phrase1}"
       times += 1
     end
