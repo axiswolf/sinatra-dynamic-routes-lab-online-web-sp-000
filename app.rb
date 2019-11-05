38d2974adbbd4c682be9fa55387757e16abd8a83
Karen V <karenvo@usc.edu>
2019-11-05 08:31:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ class App < Sinatra::Base
     @number2 = params[:number2]
     @operation = params[:operation]
       if @operation == "add"
-        @number1 + @number2
+        @number1.to_i + @number2.to_i
       elsif @operation == "subtract"
         @number1 - @number2
       elsif @operation == "divide"
